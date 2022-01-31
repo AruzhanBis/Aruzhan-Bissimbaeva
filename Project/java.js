d78f277f0db0eecc0ff0aedf122f1cc793233a01
AruzhanBis <96420097+AruzhanBis@users.noreply.github.com>
2022-01-31 20:12:27 +0600

    Add files via upload

---
+++ Project/java.js
@@ -0,0 +1,11 @@
+function multiplyBy(){
+num1 = document.getElementsById("firstNumber").value;
+num2 = document.getElementsById("secondNumber").value;
+document.getElementsById("result").innerHTML = num1* num2;
+
+}
+function divideBy(){
+num1 = document.getElementsById("firstNumber").value;
+num2 = document.getElementsById("secondNumber").value;
+document.getElementsById("result").innerHTML = num1 / num2;
+}
