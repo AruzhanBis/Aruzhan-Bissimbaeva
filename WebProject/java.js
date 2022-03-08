aa1e77977a7b1dabc7489de6d7768d2bfa6e11f9
AruzhanBis <96420097+AruzhanBis@users.noreply.github.com>
2022-03-09 01:43:26 +0600

    Add files via upload

--- WebProject/java.js
+++ WebProject/java.js
@@ -5,3 +5,61 @@ function showMenu() {
 function hideMenu() {
   navLinks.style.right = "-200px";
 }
+$(function(){
+  $("#slide").hide().show(2000);
+  });
+$(function(){
+  $("#rgslide").hide().show(1100);
+  });
+ $(function(){
+      $("#btn1h").on("click", function() {
+        $("#slide1").hide(1000);
+      });
+  });
+  $(function(){
+       $("#btn1s").on("click", function() {
+           $("#slide1").show(1000);
+       });
+         });
+
+         $(function(){
+              $("#btn2h").on("click", function() {
+                $("#slide2").hide(1000);
+              });
+          });
+          $(function(){
+               $("#btn2s").on("click", function() {
+                   $("#slide2").show(1000);
+               });
+                 });
+ $(function(){
+$("#btn3h").on("click", function() {
+  $("#slide3").hide(1000);
+});
+});
+$(function(){
+     $("#btn3s").on("click", function() {
+         $("#slide3").show(1000);
+     });       });
+
+
+     $(function(){
+          $("#btn4h").on("click", function() {
+            $("#slide4").hide(1000);
+          });
+      });
+      $(function(){
+           $("#btn4s").on("click", function() {
+               $("#slide4").show(1000);
+           });
+             });
+             $(function(){
+                  $("#btn5h").on("click", function() {
+                    $("#slide5").hide(1000);
+                  });
+              });
+              $(function(){
+                   $("#btn5s").on("click", function() {
+                       $("#slide5").show(1000);
+                   });
+                     });
